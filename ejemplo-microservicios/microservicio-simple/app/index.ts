0514beb1d52a14d874ab3dcf55ce671473691176
Martín <u201520759@upc.edu.pe>
2022-11-21 16:12:42 -0500

    Se elimino los ejemplos de microservicios y estandares

--- ejemplo-microservicios/microservicio-simple/app/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express"
-
-const app = express()
-const appId = process.env.APPID
-
-app.get('/',(req,res)=>res.send(`${appId} Hola, te encuentras ejecutando tu primer microservicio`))
-
-app.listen(app,()=>console.info(`${appId} estas escuchando desde nodo: `))
